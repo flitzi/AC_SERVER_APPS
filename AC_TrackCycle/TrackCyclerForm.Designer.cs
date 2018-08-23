43eb82811040ee6a5ba98c370d07bd919130a86f
rafffel <rafffel1@gmail.com>
2018-08-23 23:04:28 +0200

    *

--- AC_TrackCycle/TrackCyclerForm.Designer.cs
+++ AC_TrackCycle/TrackCyclerForm.Designer.cs
@@ -648,7 +648,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(233, 27);
+            this.label5.Location = new System.Drawing.Point(230, 27);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(110, 13);
             this.label5.TabIndex = 7;
